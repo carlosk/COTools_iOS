aeba00ee8f89a6f2d6ef23d979e24a136bfe1b2c
carlos <carlosk@163.com>
2013-12-23 09:16:43 +0800

    添加了AES加密

---
+++ Encryption/Encryption.h
@@ -0,0 +1,20 @@
+//
+//  Encryption.h
+//  DownloadFile
+//
+//  Created by  on 12-1-16.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class NSString;
+
+@interface NSData (Encryption)
+
+- (NSData *)AES256EncryptWithKey:(NSString *)key;   //加密
+- (NSData *)AES256DecryptWithKey:(NSString *)key;   //解密
+- (NSString *)newStringInBase64FromData;            //追加64编码
++ (NSString*)base64encode:(NSString*)str;           //同上64编码
+
+@end
