df424d817a3cacdcf01b1a82f610d76053f10efe
carlos <carlosk@163.com>
2014-01-04 15:59:45 +0800

    no msg

--- COTools.h
+++ COTools.h
@@ -4,7 +4,7 @@
 //
 //  Created by carlos on 13-9-20.
 //  Copyright (c) 2013年 carlosk. All rights reserved.
-//我平时积累起来的工具
+//我平时积累起来的工具.我已经是1.1 版了.
 
 #import <Foundation/Foundation.h>
 #import "COPhoneTool.h"
