d757729507e1d6eab9c6084094fab9701179ad4d
carlos <carlosk@163.com>
2014-01-05 10:34:14 +0800

    test

--- COTools.h
+++ COTools.h
@@ -4,7 +4,7 @@
 //
 //  Created by carlos on 13-9-20.
 //  Copyright (c) 2013年 carlosk. All rights reserved.
-//我平时积累起来的工具.我已经是1.1版了.1
+//我平时积累起来的工具.我已经是1.1版了.tests
 
 #import <Foundation/Foundation.h>
 #import <ReactiveCocoa/ReactiveCocoa.h>
