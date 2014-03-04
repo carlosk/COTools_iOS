73c86c9c05925ed178fc55c99abac4cb6f52900b
carlos <carlosk@163.com>
2014-03-04 10:00:58 +0800

    no msg

---
+++ COToolsSample/COToolsSampleTests/COToolCateTest/Base64Test.h
@@ -0,0 +1,13 @@
+//
+//  Base64Test.h
+//  COToolsSample
+//
+//  Created by carlos on 14-2-26.
+//  Copyright (c) 2014年 carlosk. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Base64Test : XCTestCase
+
+@end
---
+++ COToolsSample/COToolsSampleTests/COToolCateTest/Base64Test.m
@@ -0,0 +1,13 @@
+//
+//  Base64Test.m
+//  COToolsSample
+//
+//  Created by carlos on 14-2-26.
+//  Copyright (c) 2014年 carlosk. All rights reserved.
+//
+
+#import "Base64Test.h"
+
+@implementation Base64Test
+
+@end
